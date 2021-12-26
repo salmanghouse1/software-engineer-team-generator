ccac937f0762ca85a4161cfac7a7297933ae1b02
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-26 13:25:51 -0500

    employee object started checked for properties

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,17 +1,19 @@
-const employee = require('../lib/employee');
+const Employee = require('../lib/employee');
 // include mocks
 
 test('Testing employee class', () => {
+    const employee = new Employee('Dave', 'Hardworking Employee', '416 843 9325');
+
+
 
     // employee object needs to be instantiated here
 
     // test if employee object contains the properties listed bellow
-    expect(employee).toHaveProperty('type');
     expect(employee).toHaveProperty('name');
-    expect(employee).toHaveProperty('phoneNumber');
     expect(employee).toHaveProperty('description');
+    expect(employee).toHaveProperty('phoneNumber');
+
     // test if employee properties equals to a string
-    expect(employee.type).toEqual(expect.any(String));
     expect(employee.name).toEqual(expect.any(String));
     expect(employee.phoneNumber).toEqual(expect.any(String));
     expect(employee.description).toEqual(expect.any(String));
