2cf8a7d4c5a23eec7a0f522dea0c645398cbf1f8
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-12-26 15:51:20 -0500

    added engineer failing test

--- __tests__/engineer.test.js
+++ __tests__/engineer.test.js
@@ -9,12 +9,13 @@ test('Testing engineer class', () => {
     // employee object needs to be instantiated here
 
     // test if engineer object contains the properties listed bellow
-
+    expect(engineer).toHaveProperty('id');
     expect(engineer).toHaveProperty('name');
     expect(engineer).toHaveProperty('description');
     expect(engineer).toHaveProperty('phoneNumber');
 
     // test if engineer properties equals to a string
+    expect(engineer.id).toEqual(expect.any(Number));
     expect(engineer.name).toEqual(expect.any(String));
     expect(engineer.phoneNumber).toEqual(expect.any(String));
     expect(engineer.description).toEqual(expect.any(String));
